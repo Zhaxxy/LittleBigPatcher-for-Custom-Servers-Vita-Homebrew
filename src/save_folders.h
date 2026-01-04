7971bcdc67b22ae003d6adae7a819b885ede1710
LittleBigPatcherTeam <201650864+LittleBigPatcherTeam@users.noreply.github.com>
2026-01-04 15:37:31 +0000

    Bumped version to v4.000

--- src/save_folders.h
+++ src/save_folders.h
@@ -1,7 +1,7 @@
 #ifndef SAVE_FOLDERS_H_   /* Include guard */
 #define SAVE_FOLDERS_H_
 
-#define VERSION_NUM_STR "v3.003"
+#define VERSION_NUM_STR "v4.000"
 
 #define ROOT_DIR "ux0:/data/lbpatcher_stuff/"
 #define UPDATE_DOWNLOAD_LINK "https://github.com/LittleBigPatcherTeam/LittleBigPatcher-for-Custom-Servers-Vita-Homebrew/releases/latest/download/LittleBigPatcher_for_Custom_Servers_Vita_Homebrew.vpk"
